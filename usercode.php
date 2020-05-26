7884b0be4ba0ac450d6e4d27f134da944c8164dc
Alexander Kozienko <a.s.kozienko@gmail.com>
2020-05-26 16:16:17 +0300

    remove composer.json

--- usercode.php
+++ usercode.php
@@ -1,15 +1,6 @@
 <?php
 
-require 'vendor/autoload.php';
-
-use ASK\XmlBuilder\XmlBuilder;
-
 function handle($data) {
-    $xmlBuilder = XmlBuilder::create();
-    $xmlBuilder->element('root')->end();
-
-    echo $xmlBuilder->getXml(true);
-
     $data['foo'] = 123;
 
     return $data;
